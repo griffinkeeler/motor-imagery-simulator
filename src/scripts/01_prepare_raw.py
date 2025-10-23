1161784de67afe7c2cdb4f4ff95abee40787b7d3
Griffin <griffinbkeeler@gmail.com>
2025-10-23 13:07:05 -0500

    Added scripts to save raw data and epochs

--- src/scripts/01_prepare_raw.py
+++ src/scripts/01_prepare_raw.py
@@ -1,19 +1,13 @@
+import mne.channels
+
 from pathlib import Path
 
 from src.bci.io.loaders import create_raw
 
 
-_subject_dict = {
-    1: "aa",
-    2: "al",
-    3: "av",
-    4: "aw",
-    5: "ay",
-}
-
-
 def _save_processed_data(
-    subject: int,
+    subject: str,
+    session: str,
     l_freq=8,
     h_freq=30,
 ):
@@ -21,7 +15,6 @@ def _save_processed_data(
     Saves data to a .fif file.
 
     Args:
-        mat_file: The name of the mat file.
         subject: Number of the subject.
         l_freq: Low pass frequency.
         h_freq: High pass frequency.
@@ -31,20 +24,24 @@ def _save_processed_data(
     """
     here = Path(__file__).parents[2]
     # Creates Raw object
-    raw = create_raw(f"{here}/data/raw/data_set_IVa_{_subject_dict[subject]}.mat")
+    raw = create_raw(file_path=f"{here}/data/raw/{session}/BCICIV_calib_ds1{subject}_1000Hz.mat")
 
     # Filter raw data
     raw.filter(l_freq=l_freq, h_freq=h_freq)
 
     # Saves data to file
     raw.save(
-        f"{here}/data/interim/sub0{subject}/sub0{subject}_preproc_raw.fif",
+        f"{here}/data/interim/{session}/eval_{subject}_preproc_raw.fif",
         overwrite=True,
     )
 
+_save_processed_data(subject='a', session='calibration')
+_save_processed_data(subject='b', session='calibration')
+_save_processed_data(subject='c', session='calibration')
+_save_processed_data(subject='d', session='calibration')
+_save_processed_data(subject='e', session='calibration')
+_save_processed_data(subject='f', session='calibration')
+_save_processed_data(subject='g', session='calibration')
 
-_save_processed_data(1)
-_save_processed_data(2)
-_save_processed_data(3)
-_save_processed_data(4)
-_save_processed_data(5)
+
+
--- src/scripts/02_make_epochs.py
+++ src/scripts/02_make_epochs.py
@@ -5,17 +5,8 @@ from pathlib import Path
 from src.bci.io.loaders import create_events
 
 
-# events→epochs (tmin/tmax)→save X,y/Epochs
-_subject_dict = {
-    1: "aa",
-    2: "al",
-    3: "av",
-    4: "aw",
-    5: "ay",
-}
-
-
-def _save_epoch_data(subject: int):
+def _save_epoch_data(subject: str,
+                     session: str):
     """
     Saves the epoch data for a given subject.
 
@@ -28,32 +19,33 @@ def _save_epoch_data(subject: int):
     here = Path(__file__).parents[2]
 
     raw = mne.io.read_raw_fif(
-        f"{here}/data/interim/sub0{subject}" f"/sub0{subject}_preproc_raw.fif"
+        f"{here}/data/interim/{session}/eval_{subject}_preproc_raw.fif"
     )
     events = create_events(
-        f"{here}/data/raw/data_set_IVa_{_subject_dict[subject]}.mat"
+        f"{here}/data/raw/{session}/BCICIV_calib_ds1{subject}_1000Hz.mat"
     )
 
-    event_dict = {"right hand": 1, "foot": 2}
+    event_dict = {"right hand": 0, "left hand": 1}
 
     epochs = mne.Epochs(
         raw,
         events,
-        tmin=0.0,
-        tmax=4.0,
+        tmin=-1.5,
+        tmax=4.5,
         event_id=event_dict,
         preload=True,
         baseline=None,
     )
 
     epochs.save(
-        f"{here}/data/processed/sub0{subject}/sub0" f"{subject}_epochs-epo.fif",
+        f"{here}/data/processed/{session}/{session}_{subject}_epochs-epo.fif",
         overwrite=True,
     )
 
-
-_save_epoch_data(1)
-_save_epoch_data(2)
-_save_epoch_data(3)
-_save_epoch_data(4)
-_save_epoch_data(5)
+_save_epoch_data(subject='a', session='calibration')
+_save_epoch_data(subject='b', session='calibration')
+_save_epoch_data(subject='c', session='calibration')
+_save_epoch_data(subject='d', session='calibration')
+_save_epoch_data(subject='e', session='calibration')
+_save_epoch_data(subject='f', session='calibration')
+_save_epoch_data(subject='g', session='calibration')
